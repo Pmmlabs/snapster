7fc391f98abbcfde708310249cdd2fda31aa26ba
Pmmlabs <kilowatt36@mail.ru>
2015-09-04 23:03:13 +0300

    инкремент версии

--- snapster.user.js
+++ snapster.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @id             Snapster@vkopt
 // @name           Snapster plugin for VkOpt
-// @version        1.0
+// @version        1.1
 // @namespace      https://greasyfork.org/users/23
 // @author         Pmmlabs@github
 // @description    Плагин для VkOpt, добавляющий на сайт ВКонтакте веб-клиент Snapster
